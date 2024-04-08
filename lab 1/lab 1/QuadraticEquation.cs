4d13e1bfbd8831e2a910a3efc53d967dccf9dee6
nesterchuk.iraida03@gmail.com <nesterchuk.iraida03@gmail.com>
2024-04-08 19:30:30 +0300

    Виконала завдання 2 лабораторної роботи. Нестерчук Іраїда

---
+++ lab 1/lab 1/QuadraticEquation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class QuadraticEquation
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public bool HasRealRoots()
+        {
+            double discriminant = b * b - 4 * a * c;
+            return discriminant >= 0;
+        }
+    }
+}
+
